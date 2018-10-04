67b9c0a5d6a4253b6af860764e13f6429c6accb5
Mounika Reddy Varala <mounikareddyvarala@gmail.com>
2018-10-03 17:40:13 -0700

    Job List

---
+++ src/api/JoblistApi.js
@@ -0,0 +1,71 @@
+import delay from './delay';
+
+
+const jobsList =[
+  {
+    "jobId" : "A1234",
+    "jobTitle" : "React JS Developer",
+    "jobDescription" : "Develop UI Rich Web Applications for the customers",
+    "positionType" : "FTE",
+    "postedDate" : "2018-09-23",
+    "categories" : ["JS", "UI"],
+    "location" : {
+      "countryCode" : "US",
+      "stateCode" : "CA",
+      "city" : "Santa Clara",
+      "zipCode" : "95050",
+      "addressLine1" : "",
+      "addressLine2" : ""
+    },
+    "jobDuties" : [
+      "Expertise in ReactJs",
+      "Excellent in HTML , CSS, JS"
+    ]
+  },
+  {
+    "jobId" : "A1235",
+    "jobTitle" : "React Native Developer",
+    "jobDescription" : "Develop UI Rich Mobile Applications for the customers",
+    "positionType" : "C2C",
+    "postedDate" : "2018-09-23",
+    "categories" : ["JS", "UI"],
+    "location" : {
+      "countryCode" : "US",
+      "stateCode" : "CA",
+      "city" : "Santa Clara",
+      "zipCode" : "95050",
+      "addressLine1" : "",
+      "addressLine2" : ""
+    },
+    "jobDuties" : [
+      "Expertise in ReactNative",
+      "Experience in Android and or iOS "
+    ]
+  },
+  {
+    "jobId" : "A1236",
+    "jobTitle" : "Node JS Developer",
+    "jobDescription" : "Node JS Server Development",
+    "positionType" : "FTE",
+    "postedDate" : "2018-09-23",
+    "categories" : ["JS", "Node"],
+    "location" : {
+      "countryCode" : "US",
+      "stateCode" : "CA",
+      "city" : "Santa Clara",
+      "zipCode" : "95050",
+      "addressLine1" : "",
+      "addressLine2" : ""
+    },
+    "jobDuties" : [
+      "Expertise in NodeJs",
+      "Experience with ExpressJS"
+    ]
+  }
+];
+
+
+
+
+
+export default JoblistApi;
